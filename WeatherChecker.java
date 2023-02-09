89cb60670ff2496e4684a9cfdb7af20729e6c4e5
Bruno Celaya Montero <brunozelaia@gmail.com>
2023-02-09 12:50:41 +0100

    Subiendo código fuente

--- WeatherChecker.java
+++ WeatherChecker.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONObject;
+//import org.json.JSONObject;
 
 public class WeatherChecker {
     private static final String API_KEY = "your_api_key_here";
